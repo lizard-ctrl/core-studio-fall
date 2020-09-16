af3c421928e02f1a9253d8e1f8b1a2b4bf3cbb1d
lizard-ctrl <payde097@newschool.edu>
2020-09-15 22:47:59 -0400

    sketch1

---
+++ daily-sketches/sketch1/code.js
@@ -0,0 +1,24 @@
+var java_elem = document.getElementById("java");
+var script_elem = document.getElementById("script");
+var mouse_elem = document.getElementById("mouse");
+var move_elem = document.getElementById("move");
+
+document.onmousemove = function(event) {
+
+	java_elem.style.width = event.pageX + "px";
+	java_elem.style.height = event.pageY + "px";
+
+	script_elem.style.width = (window.innerWidth - event.pageX) + "px";
+	script_elem.style.height = event.pageY + "px";
+	script_elem.style.left = event.pageX + "px";
+
+	mouse_elem.style.width = event.pageX + "px";
+	mouse_elem.style.height = (window.innerHeight - event.pageY) + "px";
+	mouse_elem.style.top = event.pageY + "px";
+
+	move_elem.style.width = (window.innerWidth - event.pageX) + "px";
+	move_elem.style.height = (window.innerHeight - event.pageY) + "px";
+	move_elem.style.left = event.pageX + "px";
+	move_elem.style.top = event.pageY + "px";
+
+}
