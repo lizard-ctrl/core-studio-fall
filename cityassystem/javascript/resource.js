fb263218a40a30abaf3aeb4e8f383f535d3772d3
Emma Taylor <emmataylor@Emmas-MBP.home>
2020-10-22 18:47:44 -0400

    presentation

--- cityassystem/javascript/resource.js
+++ cityassystem/javascript/resource.js
@@ -3,41 +3,69 @@ var Airtable = require('airtable');
 // Select base
 var base = new Airtable({apiKey: 'key3kQAZHgzH6SAix'}).base('app550IXVpQURH61D');
 
+
 // A function that writes a memory record into the html
 base('Imported table').select({
   // Selecting the first 3 records in Grid view:
-  maxRecords: 3,
+  maxRecords:4,
   view: "Grid view"
 }).eachPage(function page(records, fetchNextPage) {
   // This function (`page`) will get called for each page of records.
 
   records.forEach(function(record) {
       console.log('Retrieved', record.get('NAME'));
+      
   });
-  var addMemoryHtml = function(record, selector) {
-    var header = document.querySelector(selector + ' h3');
+ 
+
+
+  var containerElement = document.querySelector('.list');
+
+
+  base('Imported table').select({
+    sort: [{field: "ID", direction: "desc"}],
+    view: "Grid view"
+  }).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+    // Iterate over records
+
   
-    header.innerHTML = 'NAME' + record.id;
+    records.forEach(function(record) {
+      // Create a div to contain content for this record
+      var element = document.createElement('div');
   
-    var paragraph = document.querySelector(selector + ' p');
+      // Add classes to record element
+      element.classList.add('NAME');
+      element.classList.add('additional-NAME');
   
-    paragraph.innerHTML = record.fields.Description;
-  }
+      // Add a special cl
   
-  // Select memory 1 with find()
-  base('NAME').find('4c4a6a0d-2a41-4ec1-a0ee-71349d7e5547', function(err, record) {
+      // Add heading element to record element
+      
+  
+      // Create a paragraph element
+      var paragraph = document.createElement('p');
+  
+      // Populate paragraph element html
+      paragraph.innerHTML = record.get('NAME') + " " + record.get('PHONE');
+  
+      // Add paragraph element to record element
+      element.appendChild(paragraph);
+  
+      // Add record element to container element
+      containerElement.appendChild(element);
+    });
+  
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+  
+  }, function done(err) {
     if (err) { console.error(err); return; }
-  
-    addMemoryHtml(record, '.first-memory');
   });
+
+
+
   
-  // To fetch the next page of records, call `fetchNextPage`.
-  // If there are more records, `page` will get called again.
-  // If there are no more records, `done` will get called.
-  fetchNextPage();
-
-}, function done(err) {
-  if (err) { console.error(err); return; }
 });
-
-
